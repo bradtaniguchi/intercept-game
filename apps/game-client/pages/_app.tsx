e787533b90b283212b596814a33a069429d90ce0
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-09-18 12:55:06 -0700

    update game-client

--- apps/game-client/pages/_app.tsx
+++ apps/game-client/pages/_app.tsx
@@ -1,9 +1,9 @@
+import { UserProvider } from '@auth0/nextjs-auth0';
 import { CssBaseline } from '@mui/material';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { UserProvider } from '@auth0/nextjs-auth0';
-import './styles.css';
 import { AppHeader } from '../core/app-header/app-header';
+import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
