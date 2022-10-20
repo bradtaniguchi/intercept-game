d8bac89a36dbb68590a11e9d3db633cdb2729699
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-10-21 11:56:35 +1200

    Add types for game-sessions

---
+++ libs/game/src/lib/models/game-session/calculated-game-session-state.ts
@@ -0,0 +1,66 @@
+import { BoardCellType, BoardGrid } from '../board';
+import { Plane } from '../plane';
+import { PlaneId } from '../plane/plane-id';
+import { Faction } from './faction';
+import { GameSessionStateLive } from './game-session-state';
+
+/**
+ * Represents the calculated GameSessionState for a "live" game.
+ *
+ * This should be calculated via the `getCalculatedState` function, which
+ * calculates most of the game state from the base GameSessionStateLive data.
+ *
+ * @see GameSessionStateLive
+ */
+export interface CalculatedGameSessionState extends GameSessionStateLive {
+  /**
+   * The current user who's move it is. Initially when the game starts,
+   * this should match the `firstMove` property.
+   */
+  currentMove: Faction;
+
+  /**
+   * The current "phase" the current user's move is in. Any "end-state" moves
+   * automatically end the users turn and changes focus to the opponent.
+   *
+   * TODO: move the type-def elsewhere.
+   *
+   * - takeoff - the user needs to pick one or two planes to put into flight
+   * - in-flight - the user already has 2 planes in flight
+   * - in-intercept - the user is attacking using one of their planes onto an enemy
+   * - in-ground-attack - the user is attacking the enemies ground-bases, requires
+   *   RNG roll calculations.
+   * - opponent - essentially disables all possible moves.
+   */
+  movePhase:
+    | 'takeoff'
+    | 'in-flight'
+    | 'in-intercept'
+    | 'in-ground-attack'
+    | 'opponent';
+
+  /**
+   * Object representing the status of all the planes for both factions
+   * TODO: move nested property elsewhere.
+   */
+  currentPlanes: {
+    north: {
+      inFlight: Array<PlaneId>;
+      downed: Array<PlaneId>;
+      grounded: Array<PlaneId>;
+    };
+    south: {
+      inFlight: Array<PlaneId>;
+      downed: Array<PlaneId>;
+      grounded: Array<PlaneId>;
+    };
+  };
+
+  /**
+   * The current calculated boardGrid display.
+   *
+   * This should be the primary source of truth in regards to what is
+   * displayed to the user.
+   */
+  boardGrid: BoardGrid<BoardCellType>;
+}
---
+++ libs/game/src/lib/models/game-session/game-session-state.ts
@@ -0,0 +1,124 @@
+import { PlaneCard } from '../card';
+import { Plane } from '../plane';
+import { Faction } from './faction';
+import { GameMove } from './game-move';
+
+/**
+ * The GameSessionState represents the on-going state of the game. This does not
+ * include more "static" information, such as the players participating
+ * within the game. As such consider everything within this interface as "dynamic"
+ * during a game-session.
+ *
+ * There are multiple of state, ones that are "live" and ones that are
+ * "finished". Finished games are ones that no longer can be played, where-as
+ * "live" states are games that are currently in-play. "Live" games
+ * could be represented by this interface on the client-side and server-side,
+ * and act as the single source of truth for the actual game-state.
+ *
+ * @see GameSession
+ */
+export type GameSessionState = GameSessionStateLive | GameSessionStateFinished;
+
+/**
+ * Represents the common data between a "live" and "finished" GameSessionStates
+ *
+ * @see GameSessionState
+ * @see GameSessionStateLive
+ * @see GameSessionStateFinished
+ */
+export interface GameSessionStateBase {
+  /**
+   * The list of moves made within the game. Initially starts
+   * as an empty array when no player has made a move yet.
+   */
+  moves: GameMove[];
+
+  /**
+   * Which faction will make or has made the first move.
+   * IE: "north" or "south".
+   *
+   * Which ever faction that is specified here **must** be
+   * the first move in the array.
+   */
+  firstMove: Faction;
+
+  /**
+   * List of all planes for both factions. Used primarily as a lookup.
+   * Use the `currentPlanes` property to get an overview of all planes at once.
+   *
+   * Otherwise the plane status here should represent their current status.
+   */
+  planes: {
+    north: Array<Plane>;
+    south: Array<Plane>;
+  };
+}
+
+/**
+ * Interface representing a "live" GameSessionState.
+ *
+ * Most other state is calculated via a reducer which calculates
+ * the state after each move.
+ *
+ * **Note** some of the date within this interface may move/change
+ * for security reasons.
+ *
+ * @see GameSessionState
+ */
+export interface GameSessionStateLive extends GameSessionStateBase {
+  gameSessionStateType: 'live';
+
+  /**
+   * The cards each of the users has in their hands. Unlike the original game
+   * any "discarded" cards are instantly made available to draw from again.
+   * Upon the initialization of the game, this cards must be provided, thus
+   * these can't be calculated.
+   *
+   * As such keeping track of the cards currently in the hands of the
+   * user should be enough.
+   *
+   * **Note** the card type may change, as we don't want to provide data
+   * on the opposing player's hands to the other side's computer. With
+   * exceptions to local-only play, which must have complete knowledge of the
+   * hands.
+   */
+  cards: {
+    north: PlaneCard[] | number;
+    south: PlaneCard[] | number;
+  };
+}
+
+/**
+ * Interface representing a "finished" GameSessionState
+ */
+export interface GameSessionStateFinished extends GameSessionStateBase {
+  gameSessionStateType: 'finished';
+
+  /**
+   * The cards each user had in their hands at the time of the game ends.
+   */
+  cards: {
+    north: PlaneCard[];
+    south: PlaneCard[];
+  };
+}
+
+/**
+ * Type-guard that returns if the given GameSessionState is a "live"
+ * game.
+ *
+ * TODO: could be expanded to make more extensive data checks against
+ * the actual calculated game.
+ */
+export const isGameSessionStateLive = (
+  state: GameSessionState
+): state is GameSessionStateLive => state.gameSessionStateType === 'live';
+
+/**
+ * Type-guard that returns if the given GameSessionState is a "finished"
+ * game.
+ */
+export const isGameSessionStateFinished = (
+  state: GameSessionState
+): state is GameSessionStateFinished =>
+  state.gameSessionStateType === 'finished';
