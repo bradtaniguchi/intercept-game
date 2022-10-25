4d6647f6fbf768a05a2eaa5ea67564955ffd7875
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-10-25 14:10:32 -0700

    fix compile issue

--- libs/game/src/lib/utils/game-session/get-calculated-state.ts
+++ libs/game/src/lib/utils/game-session/get-calculated-state.ts
@@ -9,6 +9,7 @@ import {
   isSouth,
 } from '../../models/game-session/faction';
 import { updatePlaneWithMove } from '../../models/plane/plane';
+import { getPlayerFaction } from '../get-player-faction';
 
 /**
  * Utility function that returns the CalculatedGameState from the
@@ -20,32 +21,38 @@ export const getCalculatedState = (
 ): CalculatedGameSessionState => {
   // The current approach is designed this way to make it
   // more "debuggable"
-  const calculatedState = state.moves.reduce(
+  const calculatedState = state.turns.reduce(
     // use a named function to help with "debugability"
-    function getCalculatedStateFunc(acc, move) {
-      acc.currentMove = getAltFaction(move.faction);
-      acc.planes = {
-        north: isNorth(move.faction)
-          ? acc.planes.north.map((plane) =>
-              plane.id === move.plane
-                ? updatePlaneWithMove({
-                    move,
-                    plane,
-                  })
-                : plane
-            )
-          : acc.planes.north,
-        south: isSouth(move.faction)
-          ? acc.planes.south.map((plane) =>
-              plane.id === move.plane
-                ? updatePlaneWithMove({
-                    move,
-                    plane,
-                  })
-                : plane
-            )
-          : acc.planes.south,
-      };
+    function getCalculatedStateFunc(acc, turn) {
+      const { moves, player } = turn; // TODO: add other logic
+      const faction = getPlayerFaction({ player, gameSessionState: state });
+
+      // use named function to help with "debugability"
+      moves.forEach(function getCalculatedStateFuncFromMove(move) {
+        acc.currentMove = getAltFaction(faction);
+        acc.planes = {
+          north: isNorth(faction)
+            ? acc.planes.north.map((plane) =>
+                plane.id === move.plane
+                  ? updatePlaneWithMove({
+                      move,
+                      plane,
+                    })
+                  : plane
+              )
+            : acc.planes.north,
+          south: isSouth(faction)
+            ? acc.planes.south.map((plane) =>
+                plane.id === move.plane
+                  ? updatePlaneWithMove({
+                      move,
+                      plane,
+                    })
+                  : plane
+              )
+            : acc.planes.south,
+        };
+      });
       // TODO: update other properties
       return acc;
     },
